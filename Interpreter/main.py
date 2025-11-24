f17ff7f70c8a4a9a48a9f1e7581b9cb33f6c4590
BrotatoBoiV2 <flamingburrito15@gmail.com>
2025-11-24 15:05:09 -0500

    Gets lines from the Spud file.

--- Interpreter/main.py
+++ Interpreter/main.py
@@ -25,5 +25,53 @@
 
 
 # read a file line by line and process it as outlined above. 
+import os
+import sys
 
+class Main:
+    def __init__(self):
+        self._isRunning = True
+        # self.tokenizer = Tokenizer()
+        # self.parser = Parser()
+        # self.analyzer = Analyzer()
+        
+    def read_file(self, filename):
+        lines = []
+        
+        with open(filename, 'r') as file:
+            for line in file.readlines():
+                lines.append(line.strip())
+            
+        return lines
+
+    def execute(self):
+        args = sys.argv
+        argc = len(args)
+        
+        if argc != 2:
+            print(f"Usage: {args[0]} file.pot")
+        
+            return False
+        
+        filename = args[1]
+        
+        if filename[-4:] != '.pot':
+            print("Please use a `.pot` file!")
+            
+            return False
+        
+        if not os.path.exists(filename):
+            print("That file does not exist!")
+            
+            return False
+        
+        file_lines = self.read_file(filename)
+        
+        print(f"Processing the file: {file_lines}")
+
+
+if __name__ == '__main__':
+    main = Main()
+
+    main.execute()
 
